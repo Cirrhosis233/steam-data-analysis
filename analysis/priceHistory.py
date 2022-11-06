06695648c259face7374cf91419598904b10d8dc
sid <sidwang2002@gmail.com>
2022-11-06 16:09:59 -0500

    add priceHistory

--- analysis/priceHistory.py
+++ analysis/priceHistory.py
@@ -2,6 +2,7 @@ from pyspark.sql import SparkSession
 from pyspark.sql.types import *
 from pyspark.sql.functions import *
 from pyspark.sql import Window
+import pyspark.sql.functions as func
 spark = SparkSession.builder.master("local[1]").getOrCreate()
 customSchema = StructType([
   StructField("date", DateType(), True),
@@ -19,7 +20,7 @@ df.show()
 w = Window.orderBy("date")
 df.withColumn("rn",row_number().over(w)).\
 withColumn("PriceChangeTo",lead(col("Price"),1).over(w)).\
-withColumn("discount", col("PriceChangeTo")/ col("Price")).\
+withColumn("discount", func.round((col("PriceChangeTo")/ col("Price"))*100, 2)).\
 where(col("Price") != col("PriceChangeTo")).\
 drop("rn","event","link","color", "val3").\
 toPandas().to_csv("Downloads/example/res/374320.csv", header = True, index = False)
